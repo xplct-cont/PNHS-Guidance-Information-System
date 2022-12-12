400501d1a7b62e7f2b4dc81e345ed0dd8b7d917e
KennSecusana <kennsecusana@gmail.com>
2022-12-12 16:14:09 +0800

    Added Effects

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -203,4 +203,56 @@
 </body>
 
 </html>
+
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
 @endsection
+
+
+
+<style scoped>
+    
+    html {
+      height:100%;
+    }
+    
+    body {
+      margin:0;
+    }
+    
+    .bg {
+      animation:slide 3s ease-in-out infinite alternate;
+      background-image: linear-gradient(-60deg, #2c70b1 50%, white 50%);
+      bottom:0;
+      left:-50%;
+      opacity:.5;
+      position:fixed;
+      right:-50%;
+      top:0;
+      z-index:-1;
+    }
+    
+    .bg2 {
+      animation-direction:alternate-reverse;
+      animation-duration:4s;
+    }
+    
+    .bg3 {
+      animation-duration:5s;
+    }
+    
+    
+    
+    @keyframes slide {
+      0% {
+        transform:translateX(-25%);
+      }
+      100% {
+        transform:translateX(25%);
+      }
+    }
+    
+    </style>
+    
+    
