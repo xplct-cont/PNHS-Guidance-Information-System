f8c1759a787e7670259a4fa8343c19e698f98cb1
xplct-cont <kennsecusana@gmail.com>
2023-03-04 14:34:29 +0800

    Cleaned

--- app/Http/Controllers/AdviserHomePageController.php
+++ app/Http/Controllers/AdviserHomePageController.php
@@ -5,23 +5,35 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
+use App\Models\User;
 
 class AdviserHomePageController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
 
         $user = DB::table('users')->whereNotNull('approved_at')->count();   
         $admin = DB::table('users')->where('admin', '1')->count();
 
-        $student11 = DB::table('students')->where('year_section', 'Grade 11 - Wisdom')
-        ->orWhere('year_section', 'Grade 11 - Charity')
-        ->orWhere('year_section', 'Grade 11 - Faith')->count();
+        $student11 = DB::table('students')->where('year_section' ,'LIKE', '%Grade 11%')->count();
+        $student12 = DB::table('students')->where('year_section' ,'LIKE', '%Grade 12%')->count();
 
-        $student12 = DB::table('students')->where('year_section', 'Grade 12 - Hope')
-        ->orWhere('year_section', 'Grade 12 - Love')->count();
+        $adviser = User::whereNotNull('approved_at')->get()->sortBy('advisory');  
+        $adviser = User::where([
+                ['approved_at', '!=', Null],
+                [function($query) use ($request){
+                    if(($adviser = $request->adviser)){
+                        $query->orWhere('name', 'LIKE', '%'. $adviser . '%')
+                        ->orWhere('advisory', 'LIKE', '%'. $adviser . '%')->get();
+                    }
+                }]
+            ])
+        
+            ->orderBy("advisory","asc")
+            ->paginate(6);  
 
-        return view('adviserpage.adviser.homepage', compact('user', 'admin', 'student11', 'student12'));
+        return view('adviserpage.adviser.homepage', compact('user', 'admin', 'student11', 'student12'),
+        ['adviser' => $adviser])->with('i',(request()->input('page',1)-1)*5);
 
 
     }
--- resources/views/adviserpage/adviser/homepage.blade.php
+++ resources/views/adviserpage/adviser/homepage.blade.php
@@ -1,46 +1,171 @@
 @extends('adviserpage.app')
 
 @section('content')
-<h1
-style="color:dimgray; font-size:22px; margin-left:20px; position:relative; top:15px; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
-HOME</h1>
+    <h1
+        style="color:dimgray; font-size:22px; margin-left:20px; position:relative; top:15px; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+        DASHBOARD</h1>
 
-    <div class="cardBox">
-        <div class="card elevation-2">
-            <div class="iconBx">
-                <ion-icon name="person"></ion-icon>
-            </div>
-            <div>
-                <div class="cardName d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">Administrator</div>
-                <div class="numbers"><span>{{ $admin }}</span></div>
-            </div>
-        </div>
-        <div class="card elevation-2">
-            <div class="iconBx">
-                <ion-icon name="people"></ion-icon>
-            </div>
-            <div>
-                <div class="cardName d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">Grade 11 Students</div>
-                <div class="numbers"><span>{{ $student11 }}</span></div>
-            </div>
-        </div>
-        <div class="card elevation-2">
-            <div class="iconBx">
-                <ion-icon name="people"></ion-icon>
-            </div>
-            <div>
-                <div class="cardName d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">Grade 12 Students</div>
-                <div class="numbers">{{ $student12 }}</div>
-            </div>
-        </div>
+    <section class="content mt-5">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $student11 }}</h3>
 
-        <div class="card elevation-2 bg-info">
-            <div class="iconBx">
-                <ion-icon name="person-add" class="text-light"></ion-icon>
+                            <p class="text-secondary">Grade 11 Students</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-secondary"></i>
+                        </div>
+                        <a href="#" class="small-box-footer bg-info text-light"><span class="text-light"></span></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $student12 }}</h3>
+
+                            <p class="text-secondary">Grade 12 Students</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-secondary"></i>
+                        </div>
+                        <a href="#" class="small-box-footer bg-info text-light"><span class="text-light"></span></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $admin }}</h3>
+
+                            <p class="text-secondary">Administrator</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-user-cog text-secondary"></i>
+                        </div>
+                        <a href="#" class="small-box-footer bg-info text-light"><span class="text-light"></span></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light text-secondary elevation-4">
+                        <div class="inner">
+                            <h3>{{ $user }}</h3>
+
+                            <p class="text-secondary">Advisers</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-user-tie text-secondary"></i>
+                        </div>
+                        <a href="#" class="small-box-footer bg-info text-light"><span class="text-light"></span></a>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div class="cardName text-light d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">Adviser</div>
-                <div class="numbers text-light"><span>{{ $user }}</span></div>
+        </div>
+    </section>
+
+    <div class="card d-flex justify-content-end col-md-12" style="padding:3px;">
+        <h2 style="color: dimgray; font-size:20px;" class="d-flex justify-content-between mb-3 mt-2 ">SHS ADVISERS
+        </h2>
+
+        <div class="card-body">
+            <div class="search" style=" margin-bottom:10px;">
+                <div class="mx-auto pull-left">
+                    <form action="{{ route('homepage') }}" method="GET" role="search">
+
+                        <div class="input-group">
+                            <span class="input-group-btn mr-2 mt-0">
+                                <button class="btn btn-info" type="submit" title="Search">
+                                    <span class="fas fa-search"></span>
+                                </button>
+                            </span>
+                            <input type="text" class="form-control mr-2" name="adviser" placeholder="Search..."
+                                id="adviser">
+                            <a href="{{ route('homepage') }}" class=" mt-0">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button" title="Refresh page">
+                                        <span class="fas fa-sync-alt"></span>
+                                    </button>
+                                </span>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <table class="table table-sm table-hover text-dark elevation-2 rounded table-borderless text-center">
+                <thead style="background-color: rgb(219, 219, 219)">
+                    <tr>
+
+
+                        <th style="text-align: center">Status</th>
+                        <th style="text-align: center">Profile Image</th>
+                        <th style="text-align: center">Name</th>
+                        <th class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell" style="text-align: center">
+                            Adviser ID</th>
+                        <th style="text-align: center">Advisory</th>
+                        <th class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell" style="text-align: center">
+                            Role</th>
+                        <th class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell" style="text-align: center">
+                            Contact No</th>
+                        <th class="d-none d-md-table-cell d-lg-table-cell d-lg-table-cell" style="text-align: center">
+                            Email</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($adviser as $item)
+                        <tr class="text-center">
+
+                            <td>
+                                @if (Cache::has('is_online' . $item->id))
+                                    <span class="logged-in bg-success btn-xs p-1 rounded"
+                                        style=" color:white; font-size">Online</span>
+                                @else
+                                    <span class="logged-in bg-secondary btn-xs p-1 rounded"
+                                        style=" color:white;">Offline</span>
+                                @endif
+                                <div style="font-size: 12px;">
+                                    @if ($item->last_seen != null)
+                                        {{ \Carbon\Carbon::parse($item->last_seen)->diffForHumans() }}
+                                    @else
+                                        No data
+                                    @endif
+                                </div>
+                            </td>
+
+                            <td> <img src="{{ asset('storage/users-avatar/' . $item->avatar) }} " width="50px"
+                                    height="50px" alt="Image" style="border-radius: 50%"></td>
+                            <td>{{ $item->name }}</td>
+                            <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">{{ $item->adviser_id }}
+                            </td>
+                            <td>{{ $item->advisory }}</td>
+                            <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">
+                                {{ $item->admin ? 'Guidance Designate' : 'Adviser' }}</td>
+                            <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">{{ $item->contact_no }}
+                            </td>
+                            <td class="d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">{{ $item->email }}
+                            </td>
+                        </tr>
+
+                    @empty
+                        <tr>
+                            <td colspan="8" class="text-dark"><span
+                                    class="fas fa-exclamation-circle text-danger"></span> No Advisers
+                                Found!</td>
+                        </tr>
+                    @endforelse
+
+                </tbody>
+            </table>
+            <div class="mt-1 d-flex justify-content-center">
+                {{ $adviser->onEachSide(1)->links() }}
             </div>
         </div>
     </div>
@@ -57,7 +182,8 @@ HOME</h1>
                     style="width: 230px; height:230px; border-radius: 50%; position:relative; top: -20px; background-color: #5bc0de; padding-left: 2px; padding-right:2px; padding-bottom:2px; padding-top: 2px;">
                 <h3 class="" style="font-size: 22px; margin:auto; position:relative; top:-15px; color:dimgray;">
                     Pangangan National High School<br></h3>
-                <h3 class="mb-3" style="font-size: 18px; margin:auto; position:relative; top:-10px; color:dimgray;">Talisay,
+                <h3 class="mb-3" style="font-size: 18px; margin:auto; position:relative; top:-10px; color:dimgray;">
+                    Talisay,
                     Calape, Bohol<br></h3>
 
             </div>
@@ -68,12 +194,6 @@ HOME</h1>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
     <style scoped>
-        /* .card{
-        background-image: url('/images/bbb.png');
-        height:100%;
-    } */
-
-
         .cardBox {
             position: relative;
             width: 100%;
