2826fb8e40aa2cc0f521b9b1ca6f807456b09e05
xplct-cont <kennsecusana@gmail.com>
2023-03-06 10:03:42 +0800

    Cleaned

--- resources/views/adviserpage/adviser/student/MyStudentAnecdotal_Record/index.blade.php
+++ resources/views/adviserpage/adviser/student/MyStudentAnecdotal_Record/index.blade.php
@@ -1,9 +1,16 @@
 @extends('adviserpage.app')
 
 @section('content')
-<div class="p-1">
-    <a class="fas fa-arrow-left" style="font-size:20px; color:blue;" href="{{ url('show-my-student/' . $student_myS->id)}}"></a>
-</div>
+    @if ($message = Session::get('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" style="color:black;">×</button>
+            <strong>{{ $message }}</strong>
+        </div>
+    @endif
+    <div class="p-1">
+        <a class="fas fa-arrow-left" style="font-size:20px; color:blue;"
+            href="{{ url('show-my-student/' . $student_myS->id) }}"></a>
+    </div>
     <h1 class="text-dark p-3"
         style="font-weight:normal; font-size: 25px; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; ">
         Anecdotal Records of {{ $student_myS->lastname }}, {{ $student_myS->firstname }} from
@@ -40,10 +47,47 @@
                             <tr>
                                 <td class="text-dark">{{ $anec_myS->observation_date_time->format('F d,  Y - g:i A') }}</td>
                                 <td class="text-dark">{{ $anec_myS->location_of_incidents }}</td>
-                                <td><a href="{{ url('/show-my-student/'.$anec_myS->student->id.'/anecdotal_record_myStudent/' . $anec_myS->id) }}"
+                                <td><a href="{{ url('/show-my-student/' . $anec_myS->student->id . '/anecdotal_record_myStudent/' . $anec_myS->id) }}"
                                         class="btn btn-xs "><i class="fas fa-search text-info"></i></a></td>
-                                <td><a href="{{ url('delete_anecdotal_record_myStudent/' . $anec_myS->id) }}"
-                                        class="btn btn-xs "><i class="text-danger fas fa-trash-alt"></i></a></td>
+                                {{-- <td><a href="{{ url('delete_anecdotal_record_myStudent/' . $anec_myS->id) }}"
+                                        class="btn btn-xs "><i class="text-danger fas fa-trash-alt"></i></a></td> --}}
+
+                                <td><a href="#" data-toggle="modal" id="anec_myS_delete_link" class="btn  btn-sm"
+                                        data-target="#anec_myS_id{{ $anec_myS->id }}"><span
+                                            class=" text-danger fas fa-trash-alt"></span></a></td>
+
+                                <div class="modal fade" id="anec_myS_id{{ $anec_myS->id }}" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog " role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel"><span
+                                                        class="fas fa-exclamation-circle text-danger"
+                                                        style="font-size: 30px;"></span> </h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+
+                                                <form
+                                                    action="{{ url('delete_anecdotal_record_myStudent/' . $anec_myS->id) }}"
+                                                    method="GET" enctype="multipart/form-data">
+                                                    @csrf
+                                                    @method('GET')
+
+                                                    <div class="container mx-auto text-dark">
+                                                        Are you sure you want to delete this permanently?
+                                                    </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="submit" class="btn btn-danger">Delete Permanently</button>
+                                            </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             </tr>
                         @empty
                             <tr>
@@ -57,8 +101,11 @@
             </div>
         </div>
     </div>
-
-
+    <script>
+        setTimeout(function() {
+            $(' .alert-dismissible').fadeOut('slow');
+        }, 1000);
+    </script>
     <style scoped>
         td {
             border: solid 1px #5bc0de;
